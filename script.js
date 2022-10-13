66fc8ecd35baf75c66624f966ecb2e6d31144697
Humam <hu666mam@gmail.com>
2022-10-13 13:56:42 +0100

    tweaks

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ convertEl.addEventListener("click", function () { //RENDER FUNCTION
     if (isNaN(inputEl.value ) ) {
         lengthEl.textContent = volumeEl.textContent = massEl.textContent = `${inputEl.value} f kerrek`
     } else if (inputEl.value === "") {
-        alert("Insert a value please")
+        alert("Insert a value please.")
     } else {
-        let meterToFeet = (Number(inputEl.value) * 3.281).toFixed(2)
+
+    let meterToFeet = (Number(inputEl.value) * 3.281).toFixed(2)
     let feetToMeter = (Number(inputEl.value) * 0.304).toFixed(2)
 
     let leterToGallon = (Number(inputEl.value) * 0.264).toFixed(2)
@@ -26,9 +27,9 @@ convertEl.addEventListener("click", function () { //RENDER FUNCTION
     
     lengthEl.textContent = `${inputEl.value} meters = ${meterToFeet} feet | ${inputEl.value} feet = ${feetToMeter} meters`
 
-    volumeEl.textContent = `${inputEl.value} meters = ${leterToGallon} feet | ${inputEl.value} feet = ${gallonToLeter} meters`
+    volumeEl.textContent = `${inputEl.value} leters = ${leterToGallon} gallons | ${inputEl.value} gallons = ${gallonToLeter} leters`
 
-    massEl.textContent = `${inputEl.value} meters = ${kiloToPound} feet | ${inputEl.value} feet = ${poundToKilo} meters`
+    massEl.textContent = `${inputEl.value} kilos = ${kiloToPound} pounds | ${inputEl.value} pounds = ${poundToKilo} kilos`
     }
     
 })
